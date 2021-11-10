3d9a5494f482177c07cb07e66b52dcbbbdf53bb7
Nick Bennett <nick@nickbenn.com>
2021-11-10 12:07:16 -0700

    GameService in progress.

--- src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Game.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Game.java
@@ -7,14 +7,21 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
 
 @SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
+@Table(
+    indexes = {
+        @Index(columnList = "poolSize")
+    }
+)
 public class Game {
 
   @Id
@@ -36,6 +43,9 @@ public class Game {
 
   @Column(nullable = false, updatable = false, length = 255)
   private String pool;
+
+  @Column(nullable = false, updatable = false)
+  private int poolSize;
 
   @Column(nullable = false, updatable = false)
   private int length;
@@ -69,6 +79,14 @@ public class Game {
 
   public void setPool(String pool) {
     this.pool = pool;
+  }
+
+  public int getPoolSize() {
+    return poolSize;
+  }
+
+  public void setPoolSize(int poolSize) {
+    this.poolSize = poolSize;
   }
 
   public int getLength() {
