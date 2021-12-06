31322854154cba818f61e64801f5b287c735f445
Nick Bennett <nick@nickbenn.com>
2021-12-06 16:47:18 -0700

    Player ranking query complete.

--- src/main/java/edu/cnm/deepdive/codebreaker/model/dao/UserRepository.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/dao/UserRepository.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.codebreaker.model.dao;
 
 import edu.cnm.deepdive.codebreaker.model.entity.User;
+import edu.cnm.deepdive.codebreaker.view.ScoreSummary;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,16 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-  Optional<User> findByOauthKey(String oauthKey);
-
-  Optional<User> findByExternalKey(UUID externalKey);
-
-  Iterable<User> getAllByOrderByDisplayNameAsc();
-
-  @Query(
-      value =
-        "SELECT "
-      + "  u.user_id, "
+  String RANKING_STATISTICS_QUERY = "SELECT "
+      + "  u.user_id AS userId, "
       + "  u.display_name AS displayName, "
       + "  u.external_key AS externalKey, "
       + "  gs.averageGuessCount, "
@@ -27,7 +20,7 @@ public interface UserRepository extends JpaRepository<User, UUID> {
       + "  SELECT "
       + "    g.user_id, "
       + "    AVG(gu.total_guesses) AS averageGuessCount, "
-      + "    AVG(DATEDIFF(MILLISECOND, gu.last_guess, gu.first_guess)) AS averageTime "
+      + "    AVG(DATEDIFF(MILLISECOND, gu.first_guess, gu.last_guess)) AS averageTime "
       + "  FROM game AS g "
       + "  INNER JOIN ("
       + "    SELECT "
@@ -44,22 +37,20 @@ public interface UserRepository extends JpaRepository<User, UUID> {
       + "    AND g.pool_size = :poolSize "
       + "    AND gu.match_count = g.length "
       + "  GROUP BY g.user_id"
-      + ") AS gs ON gs.user_id = u.user_id ",
-      nativeQuery = true
-  )
-  Iterable<ScoreSummary> getScoreSummaries(int length, int poolSize);
+      + ") AS gs ON gs.user_id = u.user_id ";
 
+  Optional<User> findByOauthKey(String oauthKey);
 
-  interface ScoreSummary {
+  Optional<User> findByExternalKey(UUID externalKey);
 
-    String getDisplayName();
+  Iterable<User> getAllByOrderByDisplayNameAsc();
 
-    UUID getExternalKey();
+  @Query(value = RANKING_STATISTICS_QUERY + "ORDER BY averageGuessCount ASC, averageTime ASC",
+      nativeQuery = true)
+  Iterable<ScoreSummary> getScoreSummariesOrderByGuessCount(int length, int poolSize);
 
-    double getAverageGuessCount();
-
-    long getAverageTime();
-
-  }
+  @Query(value = RANKING_STATISTICS_QUERY + "ORDER BY averageTime ASC, averageGuessCount ASC",
+      nativeQuery = true)
+  Iterable<ScoreSummary> getScoreSummariesOrderByTime(int length, int poolSize);
 
 }
